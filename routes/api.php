8c255f754e0d0f06f70e8380966e2c889885a627
yigit <yigittanriverdi@yandex.com>
2017-08-24 22:53:57 +0300

    Authentication router added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix' => 'v1'], function (){
+
+    Route::post('user',[
+        'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/signin',[
+        'uses' => 'AuthController@signin'
+    ]);
+});
